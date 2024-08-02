08f28a1340a2a1f7525a4fac368f4050371c4ac0
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-08-02 02:53:58 -0300

    finalizando o projeto

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-function tocaSomPom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-    console.log(idElementoAudio)
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-for (let i = 0; i < listaDeTeclas.length; i++){
+for (let i = 0; i < listaDeTeclas.length; i++) {
 
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
-    
-    tecla.onclick = () => {
-        tocaSomPom(idAudio);
-    };
 
-    tecla.onkeydown = (evento) => {
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-        if(evento.code = 'Space') {
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
-        
+
     }
 
-    tecla.onkeyup = () => {
+    tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
+
 }
