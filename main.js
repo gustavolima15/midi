9a430ad6cbac2dfa0481fc5bca88e152e4a2fe6c
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-08-01 22:42:46 -0300

    textos dinâmicos

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+    console.log(idElementoAudio)
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let count = 0;
+
+while(count < listaDeTeclas.length) {
+    const instrumento = listaDeTeclas[count].classList[1];
+    listaDeTeclas[count].onclick = () => {
+        tocaSomPom('#som_'+instrumento);
+    };
+    count++
+    console.log(count)
+}
